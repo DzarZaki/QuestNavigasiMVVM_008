3067d02e9128ee1a227bc23774811ae0e74e1fdc
DzarZaki <dzarzaki2@gmail.com>
2024-11-21 13:06:12 +0700

    Menambahkan NavHost dengan padding

--- app/src/main/java/com/example/pertemuan7/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan7/Navigasi.kt
@@ -1,11 +1,13 @@
 package com.example.pertemuan7
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.pertemuan7.ui.viewmodel.MahasiswaViewModel
 import java.lang.reflect.Modifier
@@ -24,4 +26,12 @@ fun Navigasi(
     Scaffold { isipadding ->
         val uiState by viewModel.dataModel.collectAsState()
 
+        NavHost(
+            modifier = Modifier.padding(isipadding),
+            navController = navHost,
+            startDestination = Halaman.Form.name
+        ){
+
+
+        }
     }
