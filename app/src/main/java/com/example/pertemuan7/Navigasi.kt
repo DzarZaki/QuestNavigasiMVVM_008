ecb18a400dcf222bee8de102aaeda2a114d58a5a
DzarZaki <dzarzaki2@gmail.com>
2024-11-21 13:01:21 +0700

    Menambahkan fungsi Navigasi Composable dengan parameter default

--- app/src/main/java/com/example/pertemuan7/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan7/Navigasi.kt
@@ -1,6 +1,22 @@
 package com.example.pertemuan7
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan7.ui.viewmodel.MahasiswaViewModel
+import java.lang.reflect.Modifier
+
 enum class Halaman {
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier(),
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+
 }
