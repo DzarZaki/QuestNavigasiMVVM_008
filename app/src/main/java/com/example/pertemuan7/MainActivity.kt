93ee9a2a2127415cf9f26641ce7e358a24b51bec
DzarZaki <dzarzaki2@gmail.com>
2024-11-21 13:30:57 +0700

    Memperbaiki Bug

--- app/src/main/java/com/example/pertemuan7/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan7/MainActivity.kt
@@ -22,9 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                        Navigasi(modifier = Modifier)
-                    }
+                    Navigasi()
                 }
             }
         }
